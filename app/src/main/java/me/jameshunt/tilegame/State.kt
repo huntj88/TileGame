29889114340c9f2faa0ca153adf064871def0fe4
huntj88 <huntj88@gmail.com>
2020-10-11 00:54:06 -0500

    Moved StateMachine to its own file

--- app/src/main/java/me/jameshunt/tilegame/State.kt
+++ app/src/main/java/me/jameshunt/tilegame/State.kt
@@ -3,91 +3,6 @@ package me.jameshunt.tilegame
 import me.jameshunt.tilegame.input.ExternalInput
 import me.jameshunt.tilegame.input.FallFromDirection
 import me.jameshunt.tilegame.input.TouchInput
-import java.util.concurrent.Executors
-
-class StateMachine(
-    private val externalInput: ExternalInput,
-    private val onNewStateReadyForRender: () -> Unit,
-    private val onError: (e: Throwable) -> Unit
-) {
-    private var state: State = getInitialState()
-
-    init {
-        // By putting all of the magic in a background thread we can control
-        // the rate at which the state machine progresses
-        // UI thread also needs to do less work
-        Executors.newSingleThreadExecutor().submit {
-            // super basic state loop
-            while (true) {
-                if (state.tick % externalInput.config.sleepEveryXTicks == 0) {
-                    Thread.sleep(externalInput.config.milliToSleepFor)
-                }
-
-                val lastState = state
-                val nextState = try {
-                    lastState
-                        .loadConfigChange()
-                        .getNextState()
-                } catch (t: Throwable) {
-                    onError(t)
-                    return@submit
-                }
-
-                synchronized(this) {
-                    state = nextState
-                }
-
-                if (lastState.step != Step.WaitForInput || nextState.step != Step.WaitForInput) {
-                    onNewStateReadyForRender()
-                }
-            }
-        }
-    }
-
-    fun getCurrentState(): State = synchronized(this) { state }
-
-    private fun State.loadConfigChange(): State {
-        if (this.step is Step.CheckForPoints) {
-            // defer loading config until after match evaluation
-            return this
-        }
-
-        val currentConfig = externalInput.config
-
-        fun Step.TilesFalling.handleTilesFallingConfigChange(): Step {
-            // falling state data could be stale due to different board size
-            return when (currentConfig.gridSize == this.lowestPosYOfFallableTiles.size) {
-                true -> this
-                false -> Step.CheckForFallableTiles
-            }
-        }
-
-        return this.copy(
-            config = currentConfig,
-            tiles = this.tiles.shrinkOrGrow(currentConfig.gridSize),
-            invisibleTiles = this.invisibleTiles.shrinkOrGrowFilled(
-                currentConfig.gridSize,
-                currentConfig.numTileTypes
-            ),
-            step = when (step) {
-                is Step.TilesFalling -> step.handleTilesFallingConfigChange()
-                else -> step
-            }
-        )
-    }
-
-    private fun getInitialState(): State {
-        val gridSize = externalInput.config.gridSize
-        val numTileTypes = externalInput.config.numTileTypes
-        return State(
-            tiles = getInitialBoard(gridSize, numTileTypes),
-            invisibleTiles = getInitialBoard(gridSize, numTileTypes),
-            step = Step.CheckForFallableTiles,
-            externalInput = externalInput,
-            config = externalInput.config
-        )
-    }
-}
 
 typealias TileXCoordinate = Int
 typealias TileYCoordinate = Int
@@ -133,7 +48,7 @@ data class State(
     val invisibleTiles: List<List<Tile?>>,
     val step: Step,
     val tick: Int = 0,
-    val config: GameView.Config,
+    val config: GameView.Config, // copy of the config at the time the State was created
     private val externalInput: ExternalInput,
 ) {
 
